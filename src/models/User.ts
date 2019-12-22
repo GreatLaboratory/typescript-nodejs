a60d747952201f02a61bc69420486d11450adb43
Myung-gwan Kim <cartopia95@naver.com>
2019-12-22 18:33:57 +0900

    user테이블 스키마 재정의

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,24 +1,60 @@
-import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt-nodejs';
+import { Document, Schema, Error, model } from 'mongoose';
+
+export type UserDocument = Document & {
+    email: string;
+    password: string;
+
+    facebook: string;
+    tokens: AuthToken[];
+
+    profile: {
+        name: string;
+        gender: string;
+        location: string;
+        website: string;
+        picture: string;
+    };
+
+};
+
+export interface AuthToken {
+    accessToken: string;
+    kind: string;
+}
 
 const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    age: {
-        type: Number,
-        required: true,
-    },
-    married: {
-        type: Boolean,
-        required: true,
-    },
-    comment: String,
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+    email: { type: String, unique: true },
+    password: String,
+
+    facebook: String,
+    twitter: String,
+    google: String,
+    tokens: Array,
+
+    profile: {
+        name: String,
+        gender: String,
+        location: String,
+        website: String,
+        picture: String
+    }
+}, { timestamps: true });
+
+/**
+ * Password hash middleware.
+ */
+userSchema.pre('save', function save(next) {
+    const user = this as UserDocument;
+    if (!user.isModified('password')) { return next(); }
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) { return next(err); }
+        bcrypt.hash(user.password, salt, (err: Error, hash) => {
+            if (err) { return next(err); }
+            user.password = hash;
+            next();
+        });
+    });
 });
 
-export default model('User', userSchema);
+export const User = model<UserDocument>('User', userSchema);

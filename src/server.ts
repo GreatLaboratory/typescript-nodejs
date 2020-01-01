da134795492670a4d7995f8ca9a5d130d90901d9
Myung-gwan Kim <cartopia95@naver.com>
2020-01-01 14:14:20 +0900

    서버의 db, 세션, 라우터, 미들웨어 등의 기본설정

--- src/server.ts
+++ src/server.ts
@@ -1,48 +1,54 @@
 import 'dotenv/config';
-import express, { Application } from 'express';
-import mongoose from 'mongoose';
-import morgan from 'morgan';
-import helmet from 'helmet';
+
 import compression from 'compression';
 import cors from 'cors';
 import errorHandler from 'errorhandler';
+import express, { Application } from 'express';
+import flash from 'express-flash';
+import helmet from 'helmet';
+import mongo, { MongoStoreFactory } from 'connect-mongo';
+import mongoose, { Error } from 'mongoose';
+import morgan from 'morgan';
+import passport from 'passport';
+import session from 'express-session';
 
-import testRouter from './routes/testRouter';
+import { passportConfig } from './passport';
+import authRouter from './routes/authRouter';
 import userRouter from './routes/userRouter';
+import testRouter from './routes/testRouter';
 
 // Server Class
 class Server {
     // Express App 필드 선언
-    public app: Application;
+    private app: Application;
+    private mongo_uri: string = process.env.MONGO_URI || 'mongodb://{username}:{password}@localhost:{port}';
+    private session_secret: string = process.env.SESSION_SECRET || 'NULL';
 
     // 생성자
     constructor() {
         this.app = express();
         this.connectDB();
+        passportConfig(passport);
         this.config();
         this.routes();
     }
 
     // DB 연결
-    public connectDB(): void {
-        const MONGO_URI = 'null';
-        mongoose.set('useFindAndModify', false);
-        const connect = () => {
-            mongoose.connect(process.env.MONGO_URI || MONGO_URI, {
+    private connectDB(): void {
+        const connect = async () => {
+            await mongoose.connect(this.mongo_uri, {
                 dbName : 'typescript',
+                useFindAndModify: false,
                 useNewUrlParser: true,
                 useCreateIndex: true,
                 useUnifiedTopology: true
-            }, (error) => {
-                if (error) {
-                    console.log('몽고디비 연결 에러', error);
-                } else {
-                    console.log('몽고디비 연결 성공');
-                }
+            }, (error: Error) => {
+                if (error) { console.error('몽고디비 연결 에러', error); }
+                else { console.log('몽고디비 연결 성공'); }
             });
         };
         connect();
-        mongoose.connection.on('error', (error) => {
+        mongoose.connection.on('error', (error: Error) => {
             console.log('몽고디비 연결 에러', error);
         });
         mongoose.connection.on('disconnected', () => {
@@ -52,9 +58,10 @@ class Server {
     }
 
     // 기본 서버 설정 및 미들웨어 
-    public config(): void {
+    private config(): void {
         // Settings
         this.app.set('port', process.env.PORT || 3000);
+        const MongoStore: MongoStoreFactory = mongo(session);
 
         // middlewares
         this.app.use(morgan('dev'));
@@ -63,12 +70,24 @@ class Server {
         this.app.use(helmet());
         this.app.use(compression());
         this.app.use(cors());
+        this.app.use(session({
+            resave: true,
+            saveUninitialized: false,
+            secret: this.session_secret,
+            store: new MongoStore({
+                mongooseConnection: mongoose.connection
+            })
+        }));
+        this.app.use(passport.initialize());
+        this.app.use(passport.session());
+        this.app.use(flash());
     }
 
     // 라우터
-    public routes(): void {
-        this.app.use('/test', testRouter);
+    private routes(): void {
+        this.app.use('/auth', authRouter);
         this.app.use('/user', userRouter);
+        this.app.use('/test', testRouter);
     }
 
     // 서버 구동
@@ -81,5 +100,5 @@ class Server {
     }
 }
 
-const server = new Server();
+const server: Server = new Server();
 server.start();

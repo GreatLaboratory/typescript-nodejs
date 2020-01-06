1afe0df3daa905b7bc0191b8c360df2bff35f9dd
Myung-gwan Kim <cartopia95@naver.com>
2020-01-06 11:12:36 +0900

    에러 매개변수 e -> err 변경

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,14 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 
-import { User } from '../models/User';
+import { User } from '../models/user';
 
 const getUsers = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
         const users = await User.find();
         res.status(200).json(users);   
-    } catch (e) {
-        console.error(e);
-        next(e);
+    } catch (err) {
+        console.error(err);
+        next(err);
     }
 };
 
@@ -16,9 +16,9 @@ const getUser = async (req: Request, res: Response, next: NextFunction): Promise
     try {
         const user = await User.findById(req.params.id);
         res.status(200).json(user);
-    } catch (e) {
-        console.error(e);
-        next(e);
+    } catch (err) {
+        console.error(err);
+        next(err);
     }
 };
 
@@ -27,9 +27,9 @@ const createUser = async (req: Request, res: Response, next: NextFunction): Prom
         const newUser = new User(req.body);
         await newUser.save();
         res.status(201).json(newUser);
-    } catch (e) {
-        console.error(e);
-        next(e);
+    } catch (err) {
+        console.error(err);
+        next(err);
     }
 };
 
@@ -38,9 +38,9 @@ const updateUser = async (req: Request, res: Response, next: NextFunction): Prom
         const { id } = req.params;
         const user = await User.findByIdAndUpdate(id, req.body, {new: true});
     res.status(201).json(user);
-    } catch (e) {
-        console.error(e);
-        next(e);
+    } catch (err) {
+        console.error(err);
+        next(err);
     }
 };
 
@@ -49,9 +49,9 @@ const deleteUser = async (req: Request, res: Response, next: NextFunction): Prom
         const { id } = req.params;
         const user = await User.findByIdAndRemove(id);
     res.status(201).json(user);
-    } catch (e) {
-        console.error(e);
-        next(e);
+    } catch (err) {
+        console.error(err);
+        next(err);
     }
 };
 
